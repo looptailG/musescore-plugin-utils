04fa71c35012fd904fcb5ab8349c7aa6df4336b5
looptailG <99362337+looptailG@users.noreply.github.com>
2024-05-28 09:59:44 +0200

    Updated doc

--- TuningUtils.js
+++ TuningUtils.js
@@ -1,5 +1,5 @@
 /*
-	A collection of functions for manipulating strings.
+	A collection of functions and consultants about tuning.
 	Copyright (C) 2024 Alessandro Culatti
 
 	This program is free software: you can redistribute it and/or modify
