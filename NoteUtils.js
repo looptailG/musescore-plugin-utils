371bd8c32ef7aad3cee3cd9d6ce2e8ba67547031
looptailG <99362337+looptailG@users.noreply.github.com>
2024-07-05 22:51:23 +0200

    Added getNoteLetter()

--- NoteUtils.js
+++ NoteUtils.js
@@ -17,3 +17,39 @@
 */
 
 const VERSION = "1.0.0-alpha";
+
+/**
+ * Return the english note name for the input note.
+ */
+function getNoteLetter(note, tpcMode = "tpc1")
+{
+	switch (note[tpcMode] % 7)
+	{
+		case 0:
+			return "C";
+		
+		case 2:
+		case -5:
+			return "D";
+		
+		case 4:
+		case -3:
+			return "E";
+		
+		case 6:
+		case -1:
+			return "F";
+		
+		case 1:
+		case -6:
+			return "G";
+		
+		case 3:
+		case -4:
+			return "A";
+		
+		case 5:
+		case -2:
+			return "B";
+	}
+}
