ef20c3cf18c741b9501494f62e4e1bdb239bfd20
looptailG <99362337+looptailG@users.noreply.github.com>
2024-07-06 16:50:29 +0200

    Added function for formatted date time

---
+++ DateUtils.js
@@ -0,0 +1,50 @@
+/*
+	A collection of functions for manipulating dates.
+	Copyright (C) 2024 Alessandro Culatti
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+const VERSION = "1.0.0-alpha";
+
+/**
+ * Return the current date time in a format compatible with file names.
+ */
+function getFileDateTime()
+{
+	const date = new Date();
+	const year = now.getFullYear();
+	const month = String(now.getMonth() + 1).padStart(2, '0');
+	const day = String(now.getDate()).padStart(2, '0');
+	const hours = String(now.getHours()).padStart(2, '0');
+	const minutes = String(now.getMinutes()).padStart(2, '0');
+	const seconds = String(now.getSeconds()).padStart(2, '0');
+	return `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
+}
+
+/**
+ * Return the current date time in the RFC3339 format.
+ */
+function getRFC3339DateTime()
+{
+	const date = new Date();
+	const year = now.getFullYear();
+	const month = String(now.getMonth() + 1).padStart(2, '0');
+	const day = String(now.getDate()).padStart(2, '0');
+	const hours = String(now.getHours()).padStart(2, '0');
+	const minutes = String(now.getMinutes()).padStart(2, '0');
+	const seconds = String(now.getSeconds()).padStart(2, '0');
+	const centiseconds = String(now.getMilliseconds()).padStart(3, '0').slice(0, 2);
+	return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${centiseconds}`;
+}
