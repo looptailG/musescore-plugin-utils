6faac860bfe144561089818c83c46af0902f83bc
looptailG <99362337+looptailG@users.noreply.github.com>
2024-07-06 16:54:08 +0200

    Fixed variable names

--- DateUtils.js
+++ DateUtils.js
@@ -23,13 +23,13 @@ const VERSION = "1.0.0-alpha";
  */
 function getFileDateTime()
 {
-	const date = new Date();
-	const year = now.getFullYear();
-	const month = String(now.getMonth() + 1).padStart(2, '0');
-	const day = String(now.getDate()).padStart(2, '0');
-	const hours = String(now.getHours()).padStart(2, '0');
-	const minutes = String(now.getMinutes()).padStart(2, '0');
-	const seconds = String(now.getSeconds()).padStart(2, '0');
+	const currentDate = new Date();
+	const year = currentDate.getFullYear();
+	const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+	const day = String(currentDate.getDate()).padStart(2, '0');
+	const hours = String(currentDate.getHours()).padStart(2, '0');
+	const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+	const seconds = String(currentDate.getSeconds()).padStart(2, '0');
 	return `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
 }
 
@@ -38,13 +38,13 @@ function getFileDateTime()
  */
 function getRFC3339DateTime()
 {
-	const date = new Date();
-	const year = now.getFullYear();
-	const month = String(now.getMonth() + 1).padStart(2, '0');
-	const day = String(now.getDate()).padStart(2, '0');
-	const hours = String(now.getHours()).padStart(2, '0');
-	const minutes = String(now.getMinutes()).padStart(2, '0');
-	const seconds = String(now.getSeconds()).padStart(2, '0');
-	const centiseconds = String(now.getMilliseconds()).padStart(3, '0').slice(0, 2);
+	const currentDate = new Date();
+	const year = currentDate.getFullYear();
+	const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+	const day = String(currentDate.getDate()).padStart(2, '0');
+	const hours = String(currentDate.getHours()).padStart(2, '0');
+	const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+	const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+	const centiseconds = String(currentDate.getMilliseconds()).padStart(3, '0').slice(0, 2);
 	return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${centiseconds}`;
 }
