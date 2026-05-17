e9e2bf9b804b8369420bb62e34d3985999f3883b
Alessandro Culatti <99362337+looptailG@users.noreply.github.com>
2026-05-17 14:04:14 +0200

    Added unicode accidentals dictionary

--- AccidentalUtils.js
+++ AccidentalUtils.js
@@ -16,7 +16,21 @@
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-const VERSION = "1.1.0";
+const VERSION = "1.2.0";
+
+const UNICODE_ACCIDENTALS = {
+	"FLAT": "\uE260",
+	"NATURAL": "\uE261",
+	"SHARP": "\uE262",
+	"SHARP2": "\uE263",
+	"FLAT2": "\uE264",
+	"SHARP3": "\uE265",
+	"FLAT3": "\uE266",
+	"MIRRORED_FLAT": "\uE280",
+	"MIRRORED_FLAT2": "\uE281",
+	"SHARP_SLASH": "\uE282",
+	"SHARP_SLASH4": "\uE283"
+};
 
 const ACCIDENTAL_DATA = {
 	"NONE":
